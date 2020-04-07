25265bf7b0d5efca48facb480649eba212380594
Nevio <marjanovicnevio@gmail.com>
2020-04-07 13:39:44 +0200

    add something

--- Botzilla/Controllers/Auth/AuthController.cs
+++ Botzilla/Controllers/Auth/AuthController.cs
@@ -60,7 +60,10 @@ namespace Botzilla.Api.Controllers
                 {
                     Subject = new ClaimsIdentity(new Claim[]{
                     new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                    new Claim(ClaimTypes.Name, user.Email)
+                    new Claim(ClaimTypes.Name, user.Email),
+                    new Claim(ClaimTypes.Role, user.Role)
+                    //new Claim(ClaimTypes.Role, role)
+                    
 
                 }),
                     Issuer = "https://spark.ooo",
